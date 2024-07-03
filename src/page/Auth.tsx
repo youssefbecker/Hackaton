fd4c9e9146c5917a881097cc05433a8e03d02ca9
ousmanou youssouf <youssouf.ousmanou.oumarou@etu.univ-poitiers.fr>
2024-07-03 17:52:05 +0200

    Fixing

--- src/page/Auth.tsx
+++ src/page/Auth.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import logo from './Logo.png'; // Assurez-vous d'importer le chemin correct du logo
+import logo from './Successful authentication.png'; // Assurez-vous d'importer le chemin correct du logo
 import './App.css'; // Assurez-vous que le chemin est correct
 
 export function Auth() {
@@ -15,14 +15,16 @@ export function Auth() {
             <div className="top-bar">
                 <span className="top-bar-text">eLock</span>
             </div>
+            <button className='back-button' onClick={handleBack}>Retour</button>
+
             <header className="App-header">
                 <div className="content">
-                    <img src={logo} className="App-logo" alt="logo" />
+                    <img src={logo} className="App-logo-success" alt="logo" />
                     <div className="auth-message">
-                        <h1 className="Home-message"> Bienvenue dans votre espace utilisateur</h1>
+                        <h1 className="Home-message"> Bienvenue</h1>
                     </div>
                     <div className="Success-message1">
-                        Authrntification éffectuer avec succès !
+                        Authentification effectuée avec succès !
                     </div>
                 </div>
                 
--- src/page/Failed.tsx
+++ src/page/Failed.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import logo from './Payment failed.png';
 import './App.css'; 
 
 export function Failed() {
+
+    const navigate = useNavigate();
+
+    const handleBack = () => {
+        navigate('/');
+    };
+
     return (
         <div className="App">
             <div className="top-bar">
                 <span className="top-bar-text">eLock</span>
             </div>
+            <button className='back-button' onClick={handleBack}>Retour</button>
             <header className="App-header">
                 <div className="content">
                     <img src={logo} className="App-logo_failed" alt="logo" />
