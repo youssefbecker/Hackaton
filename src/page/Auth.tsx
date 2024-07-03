3cd5a14df93e77121d148018d52f2d8586252fc0
ousmanou youssouf <youssouf.ousmanou.oumarou@etu.univ-poitiers.fr>
2024-07-03 11:47:48 +0200

    Fix

--- src/page/Auth.tsx
+++ src/page/Auth.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import logo from './logo.jpg';
+import logo from './Logo.png';
 import './App.css';
 
  export function Auth() {
     return (
         <div className="App">
+            <div className="top-bar">
+                <span className="top-bar-text">eLock</span>
+            </div>
           <header className="App-header">
              <img src={logo} className="App-logo" alt="logo" />
             <div className="App-message">
--- src/page/Home.tsx
+++ src/page/Home.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import logo from './logo.jpg';
+import logo from './Logo.png';
 import './App.css';
 
- export function Home() {
+export function Home() {
     return (
         <div className="App">
-          <header className="App-header">
-             <img src={logo} className="App-logo" alt="logo" />
-            <div className="App-message">
-              Scanner votre carte pour vous connecter
+            <div className="top-bar">
+                <span className="top-bar-text">eLock</span>
             </div>
-          </header>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <div className="App-message">
+                    Scanner votre carte pour vous connecter
+                </div>
+            </header>
         </div>
-      );
+    );
 }
